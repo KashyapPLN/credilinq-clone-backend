570abaedd5d21d274aa8ca2cf723259c8153aa80
KashyapPLN <kashyap.pln@gmail.com>
2024-11-29 17:03:42 +0530

    Added comments

--- index.js
+++ index.js
@@ -20,31 +20,28 @@ async function createConnection() {
 
 export const client = createConnection();
 
-// Set up multer storage for handling file uploads (in memory storage)
+
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 // POST route for form submission with PDF files
 app.post('/submit', upload.array('uploadedFiles', 6), async (req, res) => {
-  // Destructure other fields from req.body
-  const { companyUEN, companyName, FullName, position, email, mobile } = req.body;
 
-  // Access uploaded files directly from req.files
-  const uploadedFiles = req.body.uploadedFiles; // Files are stored in req.files array by multer
+  const { companyUEN, companyName, FullName, position, email, mobile } = req.body;
+  const uploadedFiles = req.body.uploadedFiles;
    console.log(uploadedFiles);
-  // Check if files are uploaded
+ 
   if (!uploadedFiles || uploadedFiles.length === 0) {
     return res.status(400).json({ message: 'No files uploaded' });
   }
 
-  // Manually structure files in the required format for saving to MongoDB
   const files = uploadedFiles.map(file => ({
     filename: file.originalname,
-    data: file.buffer, // Store file as Buffer in the database
+    data: file.buffer,
     contentType: file.mimetype,
   }));
 
-  // Create the form data object to save in the database
+
   const formData = {
     companyUEN,
     companyName,
@@ -52,14 +49,13 @@ app.post('/submit', upload.array('uploadedFiles', 6), async (req, res) => {
     position,
     email,
     mobile,
-    uploadedFiles: files, // Attach the files as part of the form data
+    uploadedFiles: files,
   };
 
   try {
     const db = (await client).db('credilinq-clone');
     const collection = db.collection('forms');
 
-    // Insert form data including files into the database
     const result = await collection.insertOne(formData);
     res.status(200).json({ message: 'Form submitted successfully'});
   } catch (error) {
@@ -74,7 +70,7 @@ app.get('/forms', async (req, res) => {
     const db = (await client).db('credilinq-clone');
     const collection = db.collection('forms');
     
-    // Find all documents, excluding the 'uploadedFiles' field
+ 
     const forms = await collection.find({}, { projection: { uploadedFiles: 0 } }).toArray();
 
     res.status(200).json(forms);
